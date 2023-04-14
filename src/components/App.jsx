c875ddb1cf2bd7a75c9e11faa0b8dd9d04477889
NaTaLiYa01 <nmoskalenko911@gmail.com>
2023-04-14 16:57:11 +0300

    fixes array comparisons in App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,7 +25,7 @@ export class App extends Component {
 
   componentDidUpdate(prevProps, prevState) {
     const { contacts } = this.state;
-    if (contacts !== prevState.contacts) {
+    if (contacts.length !== prevState.contacts.length) {
       localStorage.setItem('contacts', JSON.stringify(contacts));
     }
   }
